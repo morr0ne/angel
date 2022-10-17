4a0cf548bc12e12c941a18aea6c6ebb6a1dd9d15
Matilde Morrone <contact@morrone.dev>
2022-10-17 09:43:37 +0200

    Run rustfmt on generated code

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -1,6 +1,6 @@
 use color_eyre::Result;
 use generator::{Api, GlProfile, GlRegistry};
-use std::{fs, path::PathBuf};
+use std::{fs, path::PathBuf, process::Command};
 
 const GL_XML: &str = include_str!("OpenGL-Registry/xml/gl.xml");
 
@@ -13,11 +13,28 @@ fn main() -> Result<()> {
     let mut src_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
     src_dir.pop();
     src_dir.push("angel/src");
+    let file_path = src_dir.join("gl.rs");
 
     fs::write(
-        src_dir.join("gl.rs"),
+        &file_path,
         gl_registry.generate(Api::Gl, 4.6, GlProfile::Core),
     )?;
+
+    let rustfmt_status = Command::new("rustfmt")
+        .arg("--edition")
+        .arg("2021")
+        .arg(&file_path)
+        .status();
+
+    if let Ok(status) = rustfmt_status {
+        if status.success() {
+            println!("Runned rustfmt on generated code successfully")
+        } else {
+            println!("Failed to format code")
+        }
+    } else {
+        println!("Failed to format code")
+    }
 
     Ok(())
 }
