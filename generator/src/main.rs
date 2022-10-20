2d1aac0a3d5c2c142b8bed8f107043393372de6c
Federico Maria Morrone <contact@morrone.dev>
2022-10-20 20:26:31 +0200

    Added argument parsing

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use color_eyre::Result;
 use generator::{
     generator::Generator,
@@ -7,10 +8,39 @@ use std::{fs, path::PathBuf, process::Command};
 
 const GL_XML: &str = include_str!("OpenGL-Registry/xml/gl.xml");
 
+/// An overly complicated opengl generator.
+#[derive(Parser)]
+struct Args {
+    /// Path to the registry xml file, ignored if used in conjunction with --fetch.
+    #[arg(short, long)]
+    path: Option<PathBuf>,
+    /// Fetch the latest registry xml instead of using the bundled version.
+    #[arg(short, long)]
+    fetch: bool,
+    #[arg(short, long)]
+    verbose: bool,
+}
+
 fn main() -> Result<()> {
     color_eyre::install()?;
 
-    let mut gl_registry = GlRegistry::parse(GL_XML)?;
+    let args = Args::parse();
+
+    // TODO: Proper error handling instead of propagating errors.
+    let registry_xml: String = {
+        if args.fetch {
+            reqwest::blocking::get(
+                "https://github.com/KhronosGroup/OpenGL-Registry/raw/main/xml/gl.xml",
+            )?
+            .text()?
+        } else if let Some(path) = args.path {
+            fs::read_to_string(path.canonicalize()?)?
+        } else {
+            GL_XML.to_string()
+        }
+    };
+
+    let mut gl_registry = GlRegistry::parse(&registry_xml)?;
     gl_registry.reduce(Api::Gl, 4.6, GlProfile::Core);
 
     let mut src_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
