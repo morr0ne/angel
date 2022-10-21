56fd7c80e7426440e94e4a1cd9cf5ff7f187a145
Federico Maria Morrone <contact@morrone.dev>
2022-10-22 00:05:39 +0200

    Refactored generator code to be more legibale

--- generator/src/generator.rs
+++ generator/src/generator.rs
@@ -91,8 +91,21 @@ pub type GLVULKANPROCNV = extern "system" fn();"#;
 pub struct Generator;
 
 impl Generator {
+    // TODO: This function could actually fail, handle that.
     pub fn generate(registry: &GlRegistry, api: Api, version: f32, profile: GlProfile) -> String {
+        // Iterate through all the gl enums and creates the corresponding rust consts separated by a new line.
         let formated_enums = &registry.gl_enums.iter().format_with("\n", |gl_enum, f| {
+            // The name and value of the enum can be compied as is.
+            let enum_name = &gl_enum.name;
+            let enum_value = &gl_enum.value;
+
+            /*
+                The enum types is a bit more special:
+                - If it is a bitmask the type is of "GLbitfield"
+                - If the value equals to "0xFFFFFFFFFFFFFFFF" we need a u64 since "GLenum" is too small
+                - In all other cases "GLenum" should be fine
+                TODO: Check if there are other corner cases.
+            */
             let enum_type = if gl_enum.bitmask {
                 "GLbitfield"
             } else if gl_enum.value == "0xFFFFFFFFFFFFFFFF" {
@@ -103,98 +116,138 @@ impl Generator {
 
             f(&format_args!(
                 "pub const {enum_name}: {enum_type} = {enum_value};",
-                enum_name = gl_enum.name,
-                enum_value = gl_enum.value
             ))
         });
 
-        let formated_fields = &registry.gl_commands.iter().format_with(",\n", |gl_command, f| {
-            f(&format_args!(
-                r#"{command_name}: extern "system" fn({function_parameters}){function_return_type}"#,
-                command_name=gl_command.name,
-                function_parameters=&gl_command
+        // Iterate through all gl commands and format them a fields for the function loader.
+        let formated_fields = &registry
+            .gl_commands
+            .iter()
+            .format_with(",\n", |gl_command, f| {
+                // The name and the return type of the function can be copied as is.
+                let function_name = &gl_command.name;
+                let function_return_type = &gl_command.return_type;
+
+                // The function parameters are formated by simply joining togheter the gl type separeted by a coma.
+                let function_parameters = &gl_command
                     .gl_params
                     .iter()
-                    .format_with(",", |gl_param, f| f(&gl_param.gl_type)),
-                function_return_type=gl_command.return_type
-            ))
-        });
+                    .format_with(",", |gl_param, f| f(&gl_param.gl_type));
 
+                // NOTE: This needs to be on two different lines otherwise rustfmt breaks.
+                f(&format_args!(
+                    r#"{function_name}: extern "system" fn
+                    ({function_parameters}){function_return_type}"#,
+                ))
+            });
+
+        // Iterate through all gl commands and format them a fields that can be used as contructors of the function loader.
         let formated_constructor =
             &registry
                 .gl_commands
                 .iter()
                 .format_with(",\n", |gl_command, f| {
+                    // The name and the return type of the function can be copied as is.
+                    let command_name = &gl_command.name;
+                    let function_return_type = &gl_command.return_type;
+
+                    // The function parameters are formated by simply joining togheter the gl type separeted by a coma.
+                    let function_parameters = &gl_command
+                        .gl_params
+                        .iter()
+                        .format_with(",", |gl_param, f| f(&gl_param.gl_type));
+
+                    // NOTE: Thise needs to be on multiple lines otherwise rustfmt breaks.
                     f(&format_args!(
-                        // NOTE: Thise needs to be on multiple lines otherwise the formater fricks out. I'd probably be a good idea to report the bug.
                         r#"{command_name}: transmute::<*const c_void, 
-                extern "system" fn({function_parameters}){function_return_type}>
-                (load_pointer(b"{command_name}\0")?)"#,
-                        command_name = gl_command.name,
-                        function_parameters = &gl_command
-                            .gl_params
-                            .iter()
-                            .format_with(",", |gl_param, f| f(&gl_param.gl_type)),
-                        function_return_type = gl_command.return_type,
+                        extern "system" fn({function_parameters}){function_return_type}>
+                        (load_pointer(b"{command_name}\0")?)"#,
                     ))
                 });
 
-        let formated_methods = &registry.gl_commands.iter().format_with("\n", |gl_command, f| {
-            let mut brackets = String::new();
+        let formated_methods = &registry
+            .gl_commands
+            .iter()
+            .format_with("\n", |gl_command, f| {
+                // The function name is equal to the gl command name with the "gl" prefix stripped.
+                let function_name = gl_command.name.strip_prefix("gl").unwrap(); // TODO: Error handling
+                                                                                 // The function parameters are formated by putting the name and the type next to each other with a colon in between.
+                                                                                 // Each name and type pair is separated by a coma.
+                let function_parameters =
+                    &gl_command.gl_params.iter().format_with(",", |gl_param, f| {
+                        f(&format_args!("{}:{}", gl_param.name, gl_param.gl_type))
+                    });
 
-            for gl_param in &gl_command.gl_params {
-                match gl_param.gl_type.as_str() {
-                    "GLenum" => brackets.push_str("{:#X}, "),
-                    gl_type => {
-                        if gl_type.contains('*') {
-                            brackets.push_str("{:p}, ")
-                        } else {
-                            brackets.push_str("{:?}, ")
-                        }
-                    }
-                }
-            }
+                // The return type of the function can be copied as is.
+                let function_return_type = &gl_command.return_type;
 
-            brackets.pop();
-            brackets.pop();
+                // The inner function refers to the name of the field of the function loader.
+                // The inner function name can be copied as is.
+                let inner_function_name = &gl_command.name;
 
-            f(&format_args!(
-                r#"pub unsafe fn {function_name}(&self,{function_parameters}){function_return_type}
-                {{
-                    #[cfg(all(debug_assertions, feature = "tracing", feature = "trace-calls"))]
-                    trace!("Calling gl{function_name}({brackets})", {trace_parameters});
-                    (self.{inner_function})({inner_function_parameters})
-                }}"#,
-                function_name = gl_command.name.replacen("gl", "", 1),
-                function_parameters =
-                    &gl_command
-                        .gl_params
-                        .iter()
-                        .format_with(",", |gl_param, f| f(&format_args!(
-                            "{}:{}",
-                            gl_param.name, gl_param.gl_type
-                        ))),
-                function_return_type = gl_command.return_type,
-                inner_function = gl_command.name,
-                trace_parameters = &gl_command.gl_params.iter().format_with(",", |gl_param, f| {
-                    if gl_param.gl_type == "GLDEBUGPROC" {
-                        f(&format_args!(
-                            "transmute::<_, Option<fn()>>({})",
-                            gl_param.name
-                        ))
-                    } else {
-                        f(&gl_param.name)
-                    }
-                }),
-                inner_function_parameters = &gl_command
+                // The inner function parameters are formated by simply joining togheter the gl type separeted by a coma.
+                let inner_function_parameters = &gl_command
                     .gl_params
                     .iter()
-                    .format_with(",", |gl_param, f| f(&gl_param.name)),
-            ))
-        });
+                    .format_with(",", |gl_param, f| f(&gl_param.name));
+
+                // The trace macro function parameters are all passed the same way with the expection of "GLDEBUGPROC" which has an hardcoded value.
+                // TODO: Check if other parameters need special handling.
+                let trace_parameters =
+                    &gl_command.gl_params.iter().format_with(",", |gl_param, f| {
+                        if gl_param.gl_type == "GLDEBUGPROC" {
+                            f(&format_args!(
+                                "transmute::<_, Option<fn()>>({})",
+                                gl_param.name
+                            ))
+                        } else {
+                            f(&gl_param.name)
+                        }
+                    });
+
+                // This function tries its best to find which kind of brackets need to be passed to the trace macro.
+                // TODO: Wrote this at like 4am, tired and fueled by monsters. Probably needs refactoring.
+                let brackets = {
+                    let mut brackets = String::new();
+
+                    for gl_param in &gl_command.gl_params {
+                        match gl_param.gl_type.as_str() {
+                            "GLenum" => brackets.push_str("{:#X}, "),
+                            gl_type => {
+                                if gl_type.contains('*') {
+                                    brackets.push_str("{:p}, ")
+                                } else {
+                                    brackets.push_str("{:?}, ")
+                                }
+                            }
+                        }
+                    }
+
+                    brackets.pop();
+                    brackets.pop();
+
+                    brackets
+                };
+
+                // NOTE: Thise needs to be on multiple lines otherwise rustfmt breaks.
+                f(&format_args!(
+                    r#"pub unsafe fn {function_name}
+                    (&self,{function_parameters}){function_return_type}
+                    {{
+                        #[cfg(all(debug_assertions, feature = "tracing", feature = "trace-calls"))]
+                        trace!("Calling gl{function_name}({brackets})", {trace_parameters});
+                        (self.{inner_function_name})({inner_function_parameters})
+                    }}"#,
+                ))
+            });
 
         format!(
             r#"
+/*
+    DO NOT MANUALLY EDIT THIS FILE.
+    EDITING THIS FILES CAN LEAD TO SAFETY BUGS AND MEMORY CORRUPTION.
+*/            
+
 #![allow(bad_style)]
 #![allow(clippy::too_many_arguments)]
 #![allow(clippy::missing_safety_doc)]
